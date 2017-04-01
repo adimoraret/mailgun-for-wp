b757aacf9608feaf9459c96ff1d1406a7c1b7066
Adrian Moraret <adrian.moraret@yahoo.com>
2017-04-01 00:24:14 -0700

    UI dashboard design and some code refactoring

--- utils/wordpress/page/WordpressDisplayUtil.php
+++ utils/wordpress/page/WordpressDisplayUtil.php
@@ -7,30 +7,42 @@ namespace MailGunApiForWp\Utils\Wordpress\Page {
     use MailGunApiForWp\Utils\Wordpress\WordpressUtil;
 
     final class WordpressDisplayUtil {
-    
-        public static function displayFormTable($savedOptions, $inputs, $optionName, $buttons) { ?>
-            <table class="form-table">
-                <tbody> <?php
-                    foreach($inputs as $input){
-                        $dbValue = isset($savedOptions[$input->getName()]) ? $savedOptions[$input->getName()] : '';
-                        self::displayField($input, $optionName, $dbValue);
-                    } ?>
-                </tbody>
-            </table>
-            <?php self::displayButtons($buttons); ?>
+
+        public static function displayForm($form) { ?>
+            <form method="POST" action="<?php echo WordpressUtil::getFormAction(); ?>">
+                <?php
+                settings_fields($form->getOptionGroup());
+                $savedOptions = $form->getSavedOptions();
+                $inputs = $form->getInputs();
+                $buttons = $form->getButtons();
+                $optionName = $form->getOptionName();
+                self::displayFormContent($savedOptions, $inputs, $optionName, $buttons);
+                self::displaySpan("status");
+                self::displaySpinner("spinner");
+                ?>
+            </form>
         <?php }
 
-        private static function displayButtons($buttons){
-            foreach ($buttons as $button) {
-                self::displayButton($button);
+        private static function displayFormContent($savedOptions, $inputs, $optionName, $buttons) {
+            foreach($inputs as $input){
+                $dbValue = isset($savedOptions[$input->getName()]) ? $savedOptions[$input->getName()] : '';
+                self::displayField($input, $optionName, $dbValue);
             }
+            self::displayButtons($buttons);
         }
+
+        private static function displayButtons($buttons){ ?>
+            <div class="form-actions">
+            <?php foreach ($buttons as $button) {
+                self::displayButton($button);
+            } ?>
+            </div>
+        <?php }
 
         private static function displayButton($button){ ?>
             <input type="<?php echo $button->getType();?>"
                    name="<?php echo $button->getName();?>"
                    id="<?php echo $button->getId();?>"
-                   class="<?php echo $button->getClassName();?>"
                    value="<?php _e($button->getValue(), MailGunApiForWp::PLUGIN_SLUG);?>"
                     <?php echo self::generateOnClickHtmlAttribute($button); ?>/>
             <?php
@@ -44,13 +56,15 @@ namespace MailGunApiForWp\Utils\Wordpress\Page {
             return 'onclick="' . $button->getOnClick() . '"';
         }
 
-        private static function displayField($input, $optionName, $dbValue) {
-            if ($input instanceof RadioButtonGroup){
-                self::displayRadioButtonGroup($input, $optionName, $dbValue);
-            } else if ($input instanceof Input){
-                self::displayInputField($input, $optionName, $dbValue);
-            }
-        }
+        private static function displayField($input, $optionName, $dbValue) { ?>
+            <div class="row">
+            <?php if ($input instanceof RadioButtonGroup){
+                    self::displayRadioButtonGroup($input, $optionName, $dbValue);
+                } else if ($input instanceof Input){
+                    self::displayInputField($input, $optionName, $dbValue);
+                } ?>
+            </div>
+        <?php }
 
         private static function displayInputField($input, $optionName, $dbValue) {
             switch ($input->getType()) {
@@ -61,43 +75,32 @@ namespace MailGunApiForWp\Utils\Wordpress\Page {
         }
 
         private static function displayTextInputField($input, $optionName, $dbValue) {
-        ?>
-            <tr valign="top">
-                <?php self::displayLabelHeader($input->getId(), $input->getLabel(), $input->getIsRequired()); ?>
-                <td>
-                    <input type="<?php echo $input->getType();?>"
-                        class="text"
-                        name="<?php echo $optionName . '[' . $input->getName() . ']';?>"
-                        id="<?php echo $input->getId();?>"
-                        value="<?php echo $dbValue;?>"
-                        placeholder="<?php echo $input->getPlaceHolder();?>"
-                        <?php echo $input->getIsRequired() ? 'required' : '';?>/>
-                </td>
-            </tr>
+            self::displayLabelHeader($input->getId(), $input->getLabel(), $input->getIsRequired()); ?>
+                <input type="<?php echo $input->getType();?>"
+                    class="text"
+                    name="<?php echo $optionName . '[' . $input->getName() . ']';?>"
+                    id="<?php echo $input->getId();?>"
+                    value="<?php echo $dbValue;?>"
+                    placeholder="<?php echo $input->getPlaceHolder();?>"
+                    <?php echo $input->getIsRequired() ? 'required' : '';?>/>
         <?php }
 
         private static function displayCheckboxField($input, $optionName, $dbValue) {
-        ?>
-            <tr valign="top">
-                <?php self::displayLabelHeader($input->getId(), $input->getLabel(), $input->getIsRequired()); ?>
-                <td>
-                    <input type="checkbox"
-                       class="text"
-                       name="<?php echo $optionName . '[' . $input->getName() . ']';?>"
-                       id="<?php echo $input->getId();?>"
-                       value="<?php echo $input->getValue();?>"
-                       <?php echo $dbValue == $input->getValue() ? 'checked' : ''?>
-                    />
-                </td>
-            </tr>
+            self::displayLabelHeader($input->getId(), $input->getLabel(), $input->getIsRequired()); ?>
+                <input type="checkbox"
+                   class="text"
+                   name="<?php echo $optionName . '[' . $input->getName() . ']';?>"
+                   id="<?php echo $input->getId();?>"
+                   value="<?php echo $input->getValue();?>"
+                   <?php echo $dbValue == $input->getValue() ? 'checked' : ''?>
+                />
         <?php
         }
 
         private static function displayRadioButtonGroup($radioButtonGroup, $optionName, $dbValue) {
             ?>
-            <tr valign="top">
-                <th scope="row"><label><?php echo $radioButtonGroup->getLabel(); echo $radioButtonGroup->getIsRequired() ? '*' : '';?></label></th>
-                <td>
+            <div class="rowtbd">
+                <label><?php echo $radioButtonGroup->getLabel(); echo $radioButtonGroup->getIsRequired() ? '*' : '';?></label>
                     <?php foreach($radioButtonGroup->getRadioButtons() as $radioButton){ ?>
                         <label for="<?php echo $radioButton->getId() ?>"><?php echo $radioButton->getLabel() ?></label>
                         <input type="radio"
@@ -108,12 +111,11 @@ namespace MailGunApiForWp\Utils\Wordpress\Page {
                             <?php echo $dbValue == $radioButton->getValue() ? 'checked' : $radioButton->getIsChecked() ? 'checked' : ''?>/>
                     <?php }
                     ?>
-                </td>
-            </tr>
+            </div>
         <?php }
 
         private static function displayLabelHeader($inputId, $text, $isRequired) { ?>
-            <th scope="row"><label for="<?php echo $inputId; ?>"><?php echo $text; echo $isRequired ? '*' : ''; ?></label></th>
+            <label for="<?php echo $inputId; ?>"><?php echo $text; echo $isRequired ? '*' : ''; ?></label>
         <?php }
 
         public static function displaySpan($labelId) {?>
@@ -128,21 +130,6 @@ namespace MailGunApiForWp\Utils\Wordpress\Page {
                     </section>
                 </div>
             </div>
-        <?php }
-
-        public static function displayForm($form) { ?>
-            <form method="POST" action="<?php echo WordpressUtil::getFormAction(); ?>">
-                <?php
-                settings_fields($form->getOptionGroup());
-                $savedOptions = $form->getSavedOptions();
-                $inputs = $form->getInputs();
-                $buttons = $form->getButtons();
-                $optionName = $form->getOptionName();
-                self::displayFormTable($savedOptions, $inputs, $optionName, $buttons);
-                self::displaySpan("status");
-                self::displaySpinner("spinner");
-                ?>
-            </form>
         <?php }
     }
 }
