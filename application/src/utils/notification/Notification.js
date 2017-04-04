a05c6a3192c2599ca8e31e01afbfd1ebd72a0bea
Adrian Moraret <adrian.moraret@yahoo.com>
2017-04-03 23:41:16 -0700

    Added close notification button functionality

--- application/src/utils/notification/Notification.js
+++ application/src/utils/notification/Notification.js
@@ -9,30 +9,47 @@ export default class Notification {
     constructor(options) {
         this.options = options;
         this.buildNotificationElement = this.buildNotificationElement.bind(this);
+        this.getNotificationElementChildren = this.getNotificationElementChildren.bind(this);
         this.render = this.render.bind(this);
+        this.dismiss = this.dismiss.bind(this);
     }
 
     render() {
         this.notificationElement = Notification.createNotificationParentElement();
+        this.closeButton = Notification.createCloseButton();
+        this.closeButton.onclick = this.dismiss;
         this.buildNotificationElement();
         this.options.container.insertBefore(this.notificationElement, this.options.container.firstChild);
     }
 
+    dismiss(){
+        this.options.container.removeChild(this.notificationElement);
+    }
+
     buildNotificationElement() {
-        this.notificationElement.innerHTML = Notification.createNotificationInnerString(this.options.message);
+        const notificationNodes = this.getNotificationElementChildren(this.options.message);
+        notificationNodes.map(node => {this.notificationElement.appendChild(node)} );
         this.notificationElement.className = Notification.getClassName();
+    }
+
+    getNotificationElementChildren(message) {
+        return [Notification.createMessageElement(message), this.closeButton]
+    }
+
+    static createMessageElement(message){
+        let messageNode = document.createElement('span');
+        messageNode.innerHTML = message;
+        return messageNode;
     }
 
     static getClassName() {
         return "ns-box ns-bar ns-effect-slidetop ns-type-notice ns-show ns-box-success";
     }
 
-    static createNotificationInnerString(message) {
-        //let innerString = '<span>';
-        let innerString = '<span>' + message + '</span>';
-        //innerString += '</span>';
-        innerString += '<span class="ns-close"></span></div>';
-        return innerString;
+    static createCloseButton(){
+        let button = document.createElement('span');
+        button.classList.add('ns-close');
+        return button;
     }
 
     static createNotificationParentElement() {
