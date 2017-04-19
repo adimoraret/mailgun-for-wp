d481ca482bc731b59c6dd8dc44f0b6b46c4f9eb5
Adrian Moraret <adrian.moraret@yahoo.com>
2017-04-18 21:31:49 -0700

    Started implementing Mailgun API provider

--- utils/email/provider/mailgun/MailgunHttpProvider.php
+++ utils/email/provider/mailgun/MailgunHttpProvider.php
@@ -1,20 +1,44 @@
 <?php
 namespace MailGunApiForWp\Utils\Email\Provider\Mailgun {
-    class MailgunHttpProvider extends MailgunBaseProvider {
+	class MailgunHttpProvider extends MailgunBaseProvider {
+		const MAILGUN_SEND_URL = '';
+		private $domain;
+		private $apiKey;
 
-        public function __construct($from, $fromName) {
-            parent::__construct($from, $fromName);
-        }
+		public function __construct( $domain, $apiKey ) {
+			$this->domain = $domain;
+			$this->apiKey = $apiKey;
+		}
 
-        public function sendEmail($mailMessage) {
-        }
+		public function sendEmail( $mailMessage ) {
+			$requestBody = $this->convertMailMessageToRequestBody( $mailMessage );
+			$headers     = $this->createRequestHeaders();
+			$postData    = array(
+				'body'    => $requestBody,
+				'headers' => $headers
+			);
+			$url         = $this->getMailgunSendEmailUrl();
 
-        public function IsValid($mailMessage) {
-            return false;
-        }
+			return wp_remote_post( $url, $postData );
+		}
 
-        public function getValidationMessage() {
-            return "Error in MailgunHttpProvider";
-        }
-    }
+		public function IsValid( $mailMessage ) {
+			return false;
+		}
+
+		public function getValidationMessage() {
+			return "Error in MailgunHttpProvider";
+		}
+
+		private function convertMailMessageToRequestBody( $mailMessage ) {
+		}
+
+		private function createRequestHeaders() {
+		}
+
+		private function getMailgunSendEmailUrl() {
+			return "https://api.mailgun.net/v3/$this->domain/messages";
+		}
+
+	}
 }
